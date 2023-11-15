703c42b93db815fbf16dc8ed0a8616f100c0f533
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 21:08:30 +0100

    Refactor

--- run_repair_diff_linux_cuda.sh
+++ run_repair_diff_linux_cuda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEVICE="cuda"
-M0="fash_mnist_mlp_e50_l5_h128_v2.pt"
-M1="mnist_mlp_e50_l5_h128_v1.pt"
+M0="fash_mnist_mlp_e50_l5_h128_v2_cuda.pt"
+M1="mnist_mlp_e50_l5_h128_v1_cuda.pt"
 
 python REPAIR/fuse_diff.py --device $DEVICE --model0_path $M0 --model1_path $M1
