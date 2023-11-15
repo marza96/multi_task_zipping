618599f2dcfc54e473dcd1ae91f45d3e6d58f7dd
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 21:01:36 +0100

    Refactor

---
+++ run_repair_same_darwin_cuda.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DEVICE="cuda"
+M0="fash_mnist_mlp_e50_l5_h128_v1.pt"
+M1="fash_mnist_mlp_e50_l5_h128_v2.pt"
+
+python REPAIR/fuse_same.py --device $DEVICE --model0_path $M0 --model1_path $M1
