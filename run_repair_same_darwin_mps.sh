ea7ef1b44745f688f4c015dc919aa317cafcab10
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 21:09:53 +0100

    Refactor

--- run_repair_same_darwin_mps.sh
+++ run_repair_same_darwin_mps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEVICE="mps"
-M0="fash_mnist_mlp_e50_l5_h128_v1.pt"
-M1="fash_mnist_mlp_e50_l5_h128_v2.pt"
+M0="fash_mnist_mlp_e50_l5_h128_v1_mps.pt"
+M1="fash_mnist_mlp_e50_l5_h128_v2_mps.pt"
 
 python REPAIR/fuse_same.py --device $DEVICE --model0_path $M0 --model1_path $M1
