8214e0303ae6009fae5ffd0297784c5b08149b78
Haris Sikic <haris.sikic@gmail.com>
2023-11-16 01:37:08 +0100

    ref

--- run_repair_same_linux_cuda.sh
+++ run_repair_same_linux_cuda.sh
@@ -2,6 +2,6 @@
 
 DEVICE="cuda"
 M0="fash_mnist_mlp_e50_l5_h128_v1_cuda.pt"
-M1="mnist_mlp_e50_l5_h128_v2_cuda.pt"
+M1="fash_mnist_mlp_e50_l5_h128_v2_cuda.pt"
 
 python REPAIR/fuse_same.py --device $DEVICE --model0_path $M0 --model1_path $M1
