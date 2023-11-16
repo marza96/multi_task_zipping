da8cfe3d6bdf9a849e5d69b1f56d4c3f12fe9faf
Haris Sikic <haris.sikic@gmail.com>
2023-11-16 01:46:38 +0100

    Conflict resolve

--- REPAIR/neural_align_diff.py
+++ REPAIR/neural_align_diff.py
@@ -185,10 +185,10 @@ class NeuralAlignDiff:
                     m.reset_running_stats()
             
             with torch.no_grad():
-                for inputs, labels in self.loaderc:
+                for inputs, labels in self.loader0:
                     o2 = model0_tracked(inputs.to(device))
 
-                for inputs, labels in self.loaderc:
+                for inputs, labels in self.loader1:
                     o1 = model1_tracked(inputs.to(device))
 
             model0_tracked.eval()
