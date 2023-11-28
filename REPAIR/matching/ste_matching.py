STE fixing issues, identified gradient issue (Rounding errors when backproping through STE)

REPAIR/matching/ste_matching.py
@@ -13,31 +13,30 @@ from torch.nn.utils.stateless import functional_call
 
 from collections import OrderedDict
 from torchviz import make_dot
-# import torchopt
 
 
 class ModSTE(torch.autograd.Function):
     @staticmethod
-    def forward(input, w_for, b_for, w_back, b_back, w_0):
+    def forward(input, w_for, b_for, w_hat, b_hat, w_back, w_back2):
         a = input.mm(w_for.detach().t()) + b_for.detach()
 
         return a 
 
     @staticmethod
     def setup_context(ctx, inputs, output):
-        input, w_for, b_for, w_back, b_back, w_0 = inputs
-        ctx.save_for_backward(input, w_for, b_for, w_back, b_back, w_0)
+        input, w_for, b_for, w_hat, b_hat, w_back, w_back2 = inputs
+        ctx.save_for_backward(input, w_for, b_for, w_hat, b_hat, w_back, w_back2)
 
     @staticmethod
     def backward(ctx, grad_output):
-        input, w_for, b_for, w_back, b_back, w_0 = ctx.saved_tensors
+        input, w_for, b_for, w_hat, b_hat, w_back, w_back2 = ctx.saved_tensors
         grad_input = grad_weight = grad_bias = None
 
-        grad_bias   = grad_output.sum(0) * 0.5
-        grad_input  = grad_output.mm(w_0.detach())
-        grad_weight = grad_output.t().mm(input) * 0.5
+        grad_bias   = grad_output.sum(0) / 2.0
+        grad_input  = 0.5 * (grad_output.mm(w_back.detach()) + grad_output.mm(w_back2.detach()))
+        grad_weight = grad_output.t().mm(input.detach()) / 2.0
 
-        return grad_input, None, None, grad_weight, grad_bias, None
+        return grad_input, None, None, grad_weight, grad_bias, None, None
 
 
 class ModLinearStraightThroughEstimator(torch.nn.Module):
@@ -84,108 +83,23 @@ class ModLinearStraightThroughEstimator(torch.nn.Module):
             p_out = torch.arange(self.weight.shape[0])
             p_out.to(x.device)
 
-        self.w_1 = self.w_1[p_out, :]
-        self.w_1 = self.w_1[:, p_in]
-        self.b_1 = self.b_1[p_out]
+        self.w_1 = self.w_1.clone()[p_out, :]
+        self.w_1 = self.w_1.clone()[:, p_in]
+        self.b_1 = self.b_1.clone()[p_out]
 
+        # TODO INVESTIGATE NUMERIC DIFFERENCES BETEWEEN THIS
+        # AND THE REFERENCE IMPLEMENTATION
         x = ModSTE.apply(
             x, 
-            0.5 * (self.w_0 + self.w_1),
-            0.5 * (self.b_0 + self.b_1),
+            0.5 * (self.w_0.detach() + self.w_1.detach()),
+            0.5 * (self.b_0.detach() + self.b_1.detach()),
             self.weight,
             self.bias,
-            0.5 * (self.weight.detach() + self.w_1)
+            (self.weight.detach()),
+            (self.w_1.detach())
         )
         
         return x
-    
-
-
-
-
-class STE(torch.autograd.Function):
-    @staticmethod
-    def forward(input, w_for, b_for, w_back, b_back):
-        a = input.mm(w_for.detach().t()) + b_for.detach()
-
-        return a 
-
-    @staticmethod
-    def setup_context(ctx, inputs, output):
-        input, w_for, b_for, w_back, b_back = inputs
-        ctx.save_for_backward(input, w_for, b_for, w_back, b_back)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, w_for, b_for, w_back, b_back = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
-
-        grad_bias   = grad_output.sum(0)
-        grad_input  = grad_output.mm(w_back.detach())
-        grad_weight = grad_output.t().mm(input)
-
-        return grad_input, None, None, grad_weight, grad_bias
-
-
-class LinearStraightThroughEstimator(torch.nn.Module):
-    def __init__(self, layer_0, layer_1, idx, perms):
-        super().__init__()
-        
-        self.idx   = idx
-        self.perms = perms
-        self.w_0   = layer_0.weight.detach().float()
-        self.w_1   = layer_1.weight.detach().float()
-        self.b_0   = layer_0.bias.detach().float()
-        self.b_1   = layer_1.bias.detach().float()
-
-        self.weight = torch.nn.Parameter(layer_0.weight.detach().float())
-        self.bias   = torch.nn.Parameter(layer_0.bias.detach().float())
-
-        self.weight.requires_grad = True
-        self.bias.requires_grad   = True
-
-    def reset(self, layer_1, perms):
-        self.weight.requires_grad = True
-        self.bias.requires_grad   = True
-
-        self.w_1   = layer_1.weight.detach().float()
-        self.b_1   = layer_1.bias.detach().float()
-        self.perms = perms
-
-    def forward(self, input): 
-        x     = input
-        p_in  = None
-        p_out = None
-
-        if self.idx == 0:
-            p_in = torch.arange(x.shape[1]).long()
-            p_in.to(x.device)
-        elif self.idx > 0:
-            p_in = self.perms[self.idx - 1]
-            p_in.to(x.device)
-
-        if self.idx < len(self.perms):
-            p_out = self.perms[self.idx]
-            p_out.to(x.device)
-        else:
-            p_out = torch.arange(self.weight.shape[0])
-            p_out.to(x.device)
-
-        self.w_1 = self.w_1[p_out, :]
-        self.w_1 = self.w_1[:, p_in]
-        self.b_1 = self.b_1[p_out]
-
-        x = STE.apply(
-            x, 
-            0.5 * (self.w_0 + self.w_1),
-            0.5 * (self.b_0 + self.b_1),
-            0.5 * (self.w_1 + self.weight),
-            0.5 * (self.b_1 + self.bias),
-        )
-        
-        return x
-    
-
     
 
 class SteMatching:
@@ -219,28 +133,42 @@ class SteMatching:
         # optimizer = torchopt.sgd(lr=0.5, momentum=0.9, moment_requires_grad=True)
         # opt_state = None
 
-        perms = self.weight_matching(layer_indices, copy.deepcopy(netm), copy.deepcopy(net1), init_perm=perms)
 
         for iteration in range(self.epochs):
             loss_acum = 0.0
             total = 0
 
+
             for i, (images, labels) in enumerate(tqdm.tqdm(self.loader)):
                 images = images.to(self.device)
                 labels = labels.to(self.device)
-
+                
+                # PERM IN 2nd iteration for fixed seed for some
+                # reason different than in ref implementation
+                # investigate this
+                # perms = self.weight_matching(layer_indices, copy.deepcopy(netm), copy.deepcopy(net1), init_perm=perms)
+                perms = self.weight_matching(layer_indices, copy.deepcopy(netm), copy.deepcopy(net1), init_perm=perms)
                 self._reset_network(layer_indices, netm, net1.to(self.device), copy.deepcopy(perms), zero_grad=True)
+
                 # if opt_state is None:
                     # opt_state = optimizer.init(netm.state_dict())
 
                 outputs = netm(images)
-                print("OUT MY", outputs[0, :11])
+                # print("OUT MY", outputs[0, :11])
 
-                loss = loss_fn(outputs, labels)
-                loss.backward()
+                # loss = loss_fn(outputs, labels)
 
-                for perm in perms:
-                        print(perm[:11])
+                loss = torch.nn.functional.nll_loss(outputs, labels)
+                
+                gradient = torch.autograd.grad(loss, netm.parameters())
+
+
+                # for name, grad in zip(netm.named_parameters(), gradient):
+                #     print(name[0])
+                # for name, param, grad in zip(netm.named_parameters(), grad):
+                #     print(name)
+                # loss.backward()
+                
                 # if i == 1:
                 #     print("iter %d ....................." % i)
                 #     for name, param in netm.named_parameters():
@@ -257,24 +185,38 @@ class SteMatching:
                 #     print("......................................")
                 #     return
 
-                if i == 1: 
-                    print("iter %d ....................." % i)
-                    for name, param in netm.named_parameters():
-                        if param.requires_grad is False:
-                            continue
+                # if i == 2: 
+                #     for perm in perms:
+                #         print(perm[:11])
 
-                        if not "layers.10" in name:
-                            continue
+                #     print("iter %d ....................." % i)
+                #     # for name, param in netm.named_parameters():
+                #     for t, grad in zip(netm.named_parameters(), gradient):
+                #         name = t[0]
+                #         param = t[1]
+
+                #         # if param.requires_grad is False:
+                #         #     continue
+
+                #         if not "layers.6" in name:
+                #             continue
                         
-                        try:
-                            print("MY G", name, param.grad[:5, :5])
-                        except:
-                            print("MY G", name, param.grad[:5])
-                    print("......................................")
+                #         try:
+                #             print("MY G", name, grad[:5, :5])
+                #         except:
+                #             print("MY G", name, grad[:5])
+                #     print("......................................")
 
                 # GOOD LR 0.00001
-                for name, param in netm.named_parameters():
-                    new_param = param.detach() - 0.01 * param.grad.detach()
+                # for name, param in netm.named_parameters():
+                #     new_param = param.detach() - 0.01 * param.grad.detach()
+                #     param.data = new_param.detach()
+
+                for t, grad in zip(netm.named_parameters(), gradient):
+                    name = t[0]
+                    param = t[1]
+
+                    new_param = param.detach() - 0.01 * grad.detach()
                     param.data = new_param.detach()
 
                 # if i == 0:
@@ -293,8 +235,8 @@ class SteMatching:
                 #     print("......................................")
                 #     return
 
-                if i == 1:
-                    return
+                # if i == 2:
+                #     return
                 
                 if loss.mean() < best_perm_loss:
                     best_perm_loss = loss.mean()
@@ -327,17 +269,11 @@ class SteMatching:
             layer1 = net1.layers[layer_idx]
             wrapper = ModLinearStraightThroughEstimator
 
-            lst = [
-                    wrapper(layer0, layer1, i, perms), 
-                    torch.nn.ReLU()
-                ]
-            if i == 5:
+            layers.extend(
                 [
                     wrapper(layer0, layer1, i, perms), 
                     torch.nn.ReLU()
                 ]
-            layers.extend(
-                lst
             )
 
         wrapped_model.layers = torch.nn.Sequential(*layers)
@@ -350,4 +286,4 @@ class SteMatching:
 
             netm.layers[layer_idx].reset(layer1, perms)
             if zero_grad is True:
-                netm.layers.zero_grad()
+                netm.layers.zero_grad(set_to_none=True)

REPAIR/matching/weight_matching.py
@@ -89,7 +89,7 @@ class WeightMatching():
             if init_perm is not None:
                 for i in range(len(weights0) - 1):
                     perm_mats[i] = perm_to_permmat(init_perm[i])
-
+            
             for iteration in range(self.epochs):
                 progress = False
                 rperm = torch.randperm(len(layer_indices) - 1)

REPAIR/neural_align_diff_integrated_GIT_REBASIN.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 from torchviz import make_dot
 
 
-torch.set_printoptions(precision=6, sci_mode=False)
+torch.set_printoptions(precision=4, sci_mode=False)
 
 
 def flatten_params(model):
@@ -75,7 +75,7 @@ def wm_learning(model_a, model_b, train_loader, permutation_spec, dbg_perm=None)
             # projection by weight matching
             perm, pdb = weight_matching_ref(permutation_spec,
                                         train_state, flatten_params(model_b),
-                                        max_iter=1, print_flg=False, debug_perms=dbg_perm)
+                                        max_iter=100, debug_perms=dbg_perm, init_perm=perm)
 
             projected_params = apply_permutation(permutation_spec, perm, flatten_params(model_b))
 
@@ -98,7 +98,7 @@ def wm_learning(model_a, model_b, train_loader, permutation_spec, dbg_perm=None)
 
 
             output = functional_call(model_target, midpoint_params, x)
-            print("OUT LEG", output[0, :11])
+            # print("OUT LEG", output[0, :11])
 
             # make_dot(output, params=dict(list(model_target.named_parameters()))).render("leg_tv", format="png")
             
@@ -106,8 +106,7 @@ def wm_learning(model_a, model_b, train_loader, permutation_spec, dbg_perm=None)
             
             loss.backward()
 
-            for perm in pdb:
-                print(perm[:11])
+            
             # if i == 1:
             #     print("iter %d ....................." % i)
             #     for key in train_state:
@@ -120,10 +119,13 @@ def wm_learning(model_a, model_b, train_loader, permutation_spec, dbg_perm=None)
             #     print("......................................")
             #     return
 
-            if i == 1:
+            if i == 2:
+                for perm in pdb:
+                    print(perm[:11])
+
                 print("iter %d ....................." % i)
                 for key in train_state:
-                    if not "layers.10" in key:
+                    if not "layers.6" in key:
                         continue
 
                     try:
@@ -149,7 +151,7 @@ def wm_learning(model_a, model_b, train_loader, permutation_spec, dbg_perm=None)
             #     print("......................................")
             #     return
             
-            if i == 1:
+            if i == 2:
                 return
 
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
@@ -229,7 +231,7 @@ def apply_permutation(ps: PermutationSpec, perm, params):
     return ret
 
 
-def weight_matching_ref(ps: PermutationSpec, params_a, params_b, max_iter=300, debug_perms=None, init_perm=None, print_flg=True, legacy=True):
+def weight_matching_ref(ps: PermutationSpec, params_a, params_b, max_iter=300, debug_perms=None, init_perm=None, print_flg=False, legacy=True):
     """Find a permutation of `params_b` to make them match `params_a`."""
     perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]] for p, axes in ps.perm_to_axes.items()}
     device = list(params_a.values())[0].device
@@ -561,7 +563,7 @@ class NeuralAlignDiff:
             # self.permutations, _ = weight_matching_ref(ps, dct0, dct1, max_iter=iterations, debug_perms=global_perms, legacy=False)
             # for perm in self.permutations:
             #     print(perm[:11])
-            # # print("....................")
+            # print("....................")
             # self.permutations = WeightMatching(epochs=iterations, debug=True, debug_perms=global_perms,)(self.layer_indices, cl0, cl1)
             # for perm in self.permutations:
             #     print(perm[:11])
@@ -582,7 +584,7 @@ class NeuralAlignDiff:
                 epochs=10,
                 device="mps",
                 wm_kwargs={
-                    "epochs": 1,
+                    "epochs": 100,
                     "debug": False,
                     "debug_perms": global_perms
                 }