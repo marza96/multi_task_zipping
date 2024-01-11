49c09fb9bba3c39f666392be748cdf29945f3312
Haris Sikic <haris.sikic@gmail.com>
2024-01-11 12:45:19 +0100

    Enabling bnorm for VGG nets

---
+++ fuse_vgg_cifar_split_bnorm.py
@@ -0,0 +1,141 @@
+import os
+import torch
+import torchvision
+
+import torchvision.transforms as transforms
+
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import ConcatDataset
+
+from REPAIR.fuse_diff import fuse_from_cfg
+from REPAIR.net_models.models import VGG
+from REPAIR.fuse_cfg import BaseFuseCfg
+from REPAIR.matching.weight_matching import WeightMatching
+from REPAIR.matching.activation_matching import ActivationMatching
+from REPAIR.matching.ste_weight_matching import SteMatching
+
+
+def get_datasets():
+    path   = os.path.dirname(__file__)
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+        ]
+    )
+    mnistTrainSet = torchvision.datasets.CIFAR10(
+        root=path + '/data', 
+        train=True,
+        download=True, 
+        transform=transform
+    )
+
+    first_half = [
+        idx for idx, target in enumerate(mnistTrainSet.targets) 
+        if target in [0, 1, 2, 3, 4]
+    ]
+
+    second_half = [
+        idx for idx, target in enumerate(mnistTrainSet.targets) 
+        if target in [5, 6, 7, 8, 9]
+    ]  
+
+    FirstHalfLoader = torch.utils.data.DataLoader(
+        torch.utils.data.Subset(mnistTrainSet, first_half),
+        batch_size=512,
+        shuffle=True,
+        num_workers=8)
+    
+    SecondHalfLoader = torch.utils.data.DataLoader(
+        torch.utils.data.Subset(mnistTrainSet, second_half),
+        batch_size=512,
+        shuffle=True,
+        num_workers=8)
+    
+    ConcatLoader = torch.utils.data.DataLoader(
+        ConcatDataset((torch.utils.data.Subset(mnistTrainSet, first_half), torch.utils.data.Subset(mnistTrainSet, second_half))), 
+        batch_size=512,
+        shuffle=True, 
+        num_workers=8
+    )
+    
+    return FirstHalfLoader, SecondHalfLoader, ConcatLoader
+
+
+if __name__ == "__main__":
+    loader0, loader1, loaderc = get_datasets()
+
+    fuse_cfg = BaseFuseCfg(num_experiments=2, alpha_split=10)
+
+    vgg_cfg = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+    fuse_cfg.models = {
+        0: {
+            "model": VGG,
+            "args": {
+                "w": 1,
+                "cfg": vgg_cfg,
+                "classes": 10,
+                "bnorm": True
+            }
+        },
+        1: {
+            "model": VGG,
+            "args": {
+                "w": 1,
+                "cfg": vgg_cfg,
+                "classes": 10,
+                "bnorm": True
+            }
+        },
+    }
+    fuse_cfg.configs = {
+        0: {
+            "match_method": SteMatching(
+                torch.nn.functional.cross_entropy,
+                loaderc,
+                0.25,
+                WeightMatching(
+                    epochs=1000,
+                    ret_perms=True
+                ),
+                epochs=20,
+                device="cuda"
+            ),
+            "device": "cuda"
+        },
+        1: {
+            "match_method": WeightMatching(
+                epochs=1000,
+                debug=False
+            ),
+            "device": "cpu"
+        }
+    }
+    fuse_cfg.loaders = {
+        0: {
+            "loader0": loader0,
+            "loader1": loader1,
+            "loaderc": loaderc,
+        },
+        1: {
+            "loader0": loader0,
+            "loader1": loader1,
+            "loaderc": loaderc,
+        }
+    }
+    fuse_cfg.names = {
+        0: {
+            "experiment_name": "fuse_mlp_cifar_split_AM",
+            "model0_name": "vgg_cifar_split_first",
+            "model1_name": "vgg_cifar_split_second"
+        },
+        1: {
+            "experiment_name": "fuse_mlp_cifar_split_WM",
+            "model0_name": "vgg_cifar_split_first",
+            "model1_name": "vgg_cifar_split_second"
+        }
+    }
+    
+    fuse_cfg.root_path = os.path.dirname(__file__)
+
+    fuse_from_cfg(fuse_cfg)
---
+++ train_vgg_cifar_split_bnorm.py
@@ -0,0 +1,117 @@
+import os
+import torch
+import torchvision
+
+import torchvision.transforms as transforms
+
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
+
+from REPAIR.train import train_from_cfg
+from REPAIR.net_models.models import VGG
+from REPAIR.train_cfg import BaseTrainCfg
+
+
+def get_datasets():
+    path   = os.path.dirname(__file__)
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+        ]
+    )
+    mnistTrainSet = torchvision.datasets.CIFAR10(
+        root=path + '/data', 
+        train=True,
+        download=True, 
+        transform=transform
+    )
+
+    first_half = [
+        idx for idx, target in enumerate(mnistTrainSet.targets) 
+        if target in [0, 1, 2, 3, 4]
+    ]
+
+    second_half = [
+        idx for idx, target in enumerate(mnistTrainSet.targets) 
+        if target in [5, 6, 7, 8, 9]
+    ]  
+
+    FirstHalfLoader = torch.utils.data.DataLoader(
+        torch.utils.data.Subset(mnistTrainSet, first_half),
+        batch_size=128,
+        shuffle=True,
+        num_workers=8)
+    
+    SecondHalfLoader = torch.utils.data.DataLoader(
+        torch.utils.data.Subset(mnistTrainSet, second_half),
+        batch_size=128,
+        shuffle=True,
+        num_workers=8)
+    
+    return FirstHalfLoader, SecondHalfLoader
+
+
+if __name__ == "__main__":
+    loader0, loader1 = get_datasets()
+
+    train_cfg = BaseTrainCfg(num_experiments=2)
+
+    vgg_cfg = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+    train_cfg.models = {
+        0: {
+            "model": VGG,
+            "args": {
+                "w": 1,
+                "cfg": vgg_cfg,
+                "classes": 10,
+                "bnorm": True
+            }
+        },
+        1: {
+            "model": VGG,
+            "args": {
+                "w": 1,
+                "cfg": vgg_cfg,
+                "classes": 10,
+                "bnorm": True
+            }
+        }
+    }
+    train_cfg.configs = {
+        0: {
+            "loss_fn": CrossEntropyLoss(),
+            "epochs" : 50,
+            "device": "cuda",
+            "optimizer": {
+                "class": SGD,
+                "args": {
+                    "lr": 0.05,
+                    "momentum": 0.9
+                }
+            }
+        },
+        1: {
+            "loss_fn": CrossEntropyLoss(),
+            "epochs": 30,
+            "device": "cuda",
+            "optimizer": {
+                "class": SGD,
+                "args": {
+                    "lr": 0.01,
+                    "momentum": 0.9
+                }
+            }
+        }
+    }
+    train_cfg.loaders = {
+        0: loader0,
+        1: loader1
+    }
+    train_cfg.names = {
+        0: "vgg_cifar_split_first",
+        1: "vgg_cifar_split_second"
+    }
+    train_cfg.root_path = os.path.dirname(__file__)
+
+    train_from_cfg(train_cfg)
