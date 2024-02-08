a53d4bb6d0225c10985421e3cdeac9adebbf7b3d
marza1 <marza1@cuda04.tugraz.at>
2024-02-08 14:49:23 +0100

    WM using auction config change

--- fuse_vgg_cifar_split_bnorm.py
+++ fuse_vgg_cifar_split_bnorm.py
@@ -10,7 +10,7 @@ from torch.utils.data import ConcatDataset
 from REPAIR.fuse_diff import fuse_from_cfg
 from REPAIR.net_models.models import VGG
 from REPAIR.fuse_cfg import BaseFuseCfg
-from REPAIR.matching.weight_matching_gen import WeightMatching
+from REPAIR.matching.weight_matching_gpu import WeightMatching
 from REPAIR.matching.activation_matching import ActivationMatching
 from REPAIR.matching.ste_weight_matching_gen import SteMatching
 
@@ -42,19 +42,19 @@ def get_datasets():
 
     FirstHalfLoader = torch.utils.data.DataLoader(
         torch.utils.data.Subset(mnistTrainSet, first_half),
-        batch_size=256,
+        batch_size=1024,
         shuffle=True,
         num_workers=8)
     
     SecondHalfLoader = torch.utils.data.DataLoader(
         torch.utils.data.Subset(mnistTrainSet, second_half),
-        batch_size=256,
+        batch_size=1024,
         shuffle=True,
         num_workers=8)
     
     ConcatLoader = torch.utils.data.DataLoader(
         ConcatDataset((torch.utils.data.Subset(mnistTrainSet, first_half), torch.utils.data.Subset(mnistTrainSet, second_half))), 
-        batch_size=256,
+        batch_size=1024,
         shuffle=True, 
         num_workers=8
     )
@@ -67,7 +67,7 @@ if __name__ == "__main__":
 
     fuse_cfg = BaseFuseCfg(num_experiments=4, alpha_split=20)
     
-    fuse_cfg.proj_name = "vgg_cifar_split_bnorm_log"
+    fuse_cfg.proj_name = "fuse_vgg_cifar_split_bnorm_auct"
 
     vgg_cfg = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
     fuse_cfg.models = {
@@ -110,30 +110,62 @@ if __name__ == "__main__":
     }
     fuse_cfg.configs = {
         0: {
-            "match_method": WeightMatching(
-                epochs=1000,
-                debug=False
+            "match_method": SteMatching(
+                torch.nn.functional.cross_entropy,
+                loaderc,
+                0.025,
+                WeightMatching(
+                    epochs=50,
+                    ret_perms=True,
+                    device="cuda"
+                ),
+                epochs=20,
+                device="cuda"
             ),
             "device": "cuda"
         },
         1: {
-            "match_method": WeightMatching(
-                epochs=1000,
-                debug=False
+            "match_method": SteMatching(
+                torch.nn.functional.cross_entropy,
+                loaderc,
+                0.25,
+                WeightMatching(
+                    epochs=50,
+                    ret_perms=True,
+                    device="cuda"
+                ),
+                epochs=20,
+                device="cuda"
             ),
             "device": "cuda"
         },
         2: {
-            "match_method": WeightMatching(
-                epochs=1000,
-                debug=False
+            "match_method": SteMatching(
+                torch.nn.functional.cross_entropy,
+                loaderc,
+                0.025,
+                WeightMatching(
+                    epochs=100,
+                    ret_perms=True,
+                    device="cuda"
+                ),
+                epochs=20,
+                device="cuda"
             ),
             "device": "cuda"
         },
         3: {
-            "match_method": WeightMatching(
-                epochs=1000,
-                debug=False
+            "match_method": SteMatching(
+                torch.nn.functional.cross_entropy,
+                loaderc,
+                0.25,
+                WeightMatching(
+                    epochs=100,
+                    ret_perms=True,
+                    device="cuda"
+                ),
+                epochs=20,
+                device="cuda"
             ),
             "device": "cuda"
         }
@@ -162,24 +194,24 @@ if __name__ == "__main__":
     }
     fuse_cfg.names = {
         0: {
-            "experiment_name": "fuse_mlp_cifar_split_WM_0",
-            "model0_name": "vgg_cifar_split_first_bnorm_0",
-            "model1_name": "vgg_cifar_split_second_bnorm_0"
+            "experiment_name": "fuse_mlp_cifar_split_WM_4",
+            "model0_name": "vgg_cifar_split_first_bnorm_4",
+            "model1_name": "vgg_cifar_split_second_bnorm_4"
         },
         1: {
-            "experiment_name": "fuse_mlp_cifar_split_WM_1",
-            "model0_name": "vgg_cifar_split_first_bnorm_1",
-            "model1_name": "vgg_cifar_split_second_bnorm_1"
+            "experiment_name": "fuse_mlp_cifar_split_WM_4",
+            "model0_name": "vgg_cifar_split_first_bnorm_4",
+            "model1_name": "vgg_cifar_split_second_bnorm_4"
         },
         2: {
-            "experiment_name": "fuse_mlp_cifar_split_WM_2",
-            "model0_name": "vgg_cifar_split_first_bnorm_2",
-            "model1_name": "vgg_cifar_split_second_bnorm_2"
+            "experiment_name": "fuse_mlp_cifar_split_WM_4",
+            "model0_name": "vgg_cifar_split_first_bnorm_4",
+            "model1_name": "vgg_cifar_split_second_bnorm_4"
         },
         3: {
-            "experiment_name": "fuse_mlp_cifar_split_WM_3",
-            "model0_name": "vgg_cifar_split_first_bnorm_3",
-            "model1_name": "vgg_cifar_split_second_bnorm_3"
+            "experiment_name": "fuse_mlp_cifar_split_WM_4",
+            "model0_name": "vgg_cifar_split_first_bnorm_4",
+            "model1_name": "vgg_cifar_split_second_bnorm_4"
         }
     }
     
